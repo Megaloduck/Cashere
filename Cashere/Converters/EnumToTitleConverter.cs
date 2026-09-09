4c9e65f82c90fcec8ce9af37e95d13993621d93e
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2026-09-09 20:56:14 +0700

    improving mobile sidebar UI

---
+++ Cashere/Converters/EnumToTitleConverter.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Data.Converters;
+using Cashere.ViewModels.Mobile;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashere.Converters
+{
+    public class EnumToTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MobileSection section)
+            {
+                return section switch
+                {
+                    MobileSection.Pairing => "PAIRING",
+                    MobileSection.Scanning => "SCANNING",
+                    MobileSection.Labeling => "LABELING",
+                    _ => string.Empty
+                };
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
